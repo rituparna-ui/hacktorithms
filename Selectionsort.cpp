86ffb4e687fe7a8ca226611ea7c3679f5a130ed0
Harshgupta5901 <75782910+Harshgupta5901@users.noreply.github.com>
2021-10-03 16:30:34 +0530

    Added Selectionsort.cpp

---
+++ Selectionsort.cpp
@@ -0,0 +1,54 @@
+// Selection Sort
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void swap(int *xp, int *yp)
+{
+    int temp = *xp;
+    *xp = *yp;
+    *yp = temp;
+}
+
+void selectionSort(int arr[], int n)
+{
+    int i, j, min_index;
+
+    
+    for (i = 0; i < n - 1; i++)
+    {
+        min_index = i;
+        for (j = i + 1; j < n; j++)
+        {
+            if (arr[j] < arr[min_index])
+                min_index = j;
+        }
+        swap(&arr[min_index], &arr[i]);
+    }
+}
+
+/* Function to print an array */
+void printArray(int arr[], int size)
+{
+    int i;
+    for (i = 0; i < size; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+}
+
+int main()
+{
+    int n;
+    cout << "Enter the number of elements in the array: ";
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    selectionSort(arr, n);
+    cout << "Sorted array: \n";
+    printArray(arr, n);
+    return 0;
+}
+
