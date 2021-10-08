4fb277e8908a2f070f131caf2a7b28179576b216
amvarsh <56351732+amvarsh@users.noreply.github.com>
2021-10-08 14:34:10 +0530

    Add files via upload

---
+++ n/nearlyLuckyNumber.py
@@ -0,0 +1,24 @@
+'''
+Problem 110. A. Nearly Lucky Number
+
+Petya loves lucky numbers. We all know that lucky numbers are the positive integers whose decimal representations contain only the lucky digits 4 and 7. For example, numbers 47, 744, 4 are lucky and 5, 17, 467 are not.
+
+Unfortunately, not all numbers are lucky. Petya calls a number nearly lucky if the number of lucky digits in it is a lucky number. He wonders whether number n is a nearly lucky number.
+
+Input
+The only line contains an integer n (1 ≤ n ≤ 1018).
+
+Please do not use the %lld specificator to read or write 64-bit numbers in С++. It is preferred to use the cin, cout streams or the %I64d specificator.
+
+Output
+Print on the single line "YES" if n is a nearly lucky number. Otherwise, print "NO" (without the quotes).
+
+'''
+
+
+
+n=list(input())
+if n.count('4')+n.count('7') in [4,7]:
+    print("YES")
+else:
+    print("NO")
