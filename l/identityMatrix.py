820334b79b75fceee0a0f62e803fc144b7e35d93
Rituparna W <rituparnaw16@gmail.com>
2022-10-01 16:56:08 +0530

    Initial Commit 2022

--- l/identityMatrix.py
+++
@@ -1,8 +0,0 @@
-n=int(input("Enter a number: "))
-for i in range(0,n):
-    for j in range(0,n):
-        if(i==j):
-            print("1",sep=" ",end=" ")
-        else:
-            print("0",sep=" ",end=" ")
-    print()
--- m/mumber.py
+++
@@ -1,6 +0,0 @@
-n=int(input("Enter a number n: "))
-temp=str(n)
-t1=temp+temp
-t2=temp+temp+temp
-comp=n+int(t1)+int(t2)
-print("The value is:",comp)
